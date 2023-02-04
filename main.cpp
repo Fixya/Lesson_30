134a1984f7d679df743225ec55475aa2504e16b2
Fixya <114008002+Fixya@users.noreply.github.com>
2023-02-04 19:39:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include "settings.h"
+#include "functoins.h"
+#include "game.h"
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(
+		VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE, Style::Titlebar | Style::Close);
+	window.setPosition(Vector2i{ (1920 - (int)WINDOW_WIDTH) / 2, 0 });
+	window.setFramerateLimit(FPS);
+	
+	RoadLayer grass, sand, road;
+	initRoadL(grass, Vector2f{ 0.f,0.f }, "grass.jpg");
+	initRoadL(sand, Vector2f{ 70.f,0.f }, "sand.jpg");
+	initRoadL(road, Vector2f{ 100.f,0.f }, "road.jpg");
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(grass.sprite);
+		window.draw(sand.sprite);
+		window.draw(road.sprite);
+		window.display();
+	}
+
+	return 0;
+}
---
+++ settings.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+const float WINDOW_WIDTH = 800;
+const float WINDOW_HEIGHT = 1000;
+const std::string WINDOW_TITLE = "Race to the bottom";
+const float FPS = 60.f;
+
