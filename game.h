7ad69f1aa912e72357e0c348d68d07b2a1f46581
Fixya <114008002+Fixya@users.noreply.github.com>
2023-02-11 17:06:08 +0300

    Add files via upload

--- game.h
+++ game.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "SFML/Graphics.hpp"
 #include "settings.h"
 
@@ -8,9 +8,39 @@ struct RoadLayer
 	sf::Sprite sprite;
 };
 
+struct all_text
+{
+	sf::Text  text[4];
+	int hp = 3, point = 0;
+	sf::Font font;
+
+};
+
 void initRoadL(RoadLayer& layer, sf::Vector2f pos, std::string fileName)
 {
 	layer.texture.loadFromFile(fileName);
 	layer.sprite.setTexture(layer.texture);
 	layer.sprite.setPosition(pos);
+}
+
+void textInit(all_text& text, all_text& font)
+{
+	font.font.loadFromFile("ds-digib.ttf");
+	for (int i = 0; i < 4; i++)
+	{
+		text.text[i].setFont(font.font);
+		text.text[i].setCharacterSize(30);
+	}
+		text.text[0].setPosition(textPosition);
+		text.text[1].setPosition(textPosition1);
+		text.text[2].setPosition(textPosition2);
+		text.text[3].setPosition(textPosition3);
+}
+
+void PText1(all_text& text, std::int16_t tim, all_text& hp, all_text& point)
+{
+	text.text[0].setString(std::to_string(tim));
+	text.text[1].setString("Hp");
+	text.text[2].setString(std::to_string(hp.hp));
+	text.text[3].setString(std::to_string(point.point));
 }
--- settings.h
+++ settings.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <SFML/Graphics.hpp>
 
 const float WINDOW_WIDTH = 800;
@@ -6,4 +6,9 @@ const float WINDOW_HEIGHT = 1000;
 const std::string WINDOW_TITLE = "Race to the bottom";
 const float FPS = 60.f;
 
-const sf::Vector2f START { WINDOW_WIDTH/2, WINDOW_HEIGHT - 550};
+const sf::Vector2f START{ WINDOW_WIDTH / 2, WINDOW_HEIGHT - 550 };
+
+const sf::Vector2f textPosition{ WINDOW_WIDTH - 100.f, 50 };
+const sf::Vector2f textPosition1{ WINDOW_WIDTH - 100.f, 100 };
+const sf::Vector2f textPosition2{ WINDOW_WIDTH - 100.f, 150 };
+const sf::Vector2f textPosition3{ WINDOW_WIDTH - 100.f, 200 };
