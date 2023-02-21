462ffbbc5a384d49ebddd4084d6b583f385c7e10
Fixya <114008002+Fixya@users.noreply.github.com>
2023-02-21 16:35:01 +0300

    Add files via upload

--- game.h
+++ game.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "SFML/Graphics.hpp"
 #include "settings.h"
 
@@ -10,7 +10,7 @@ struct RoadLayer
 
 struct all_text
 {
-	sf::Text  text[4];
+	sf::Text  text[6];
 	int hp = 3, point = 0;
 	sf::Font font;
 
@@ -26,21 +26,26 @@ void initRoadL(RoadLayer& layer, sf::Vector2f pos, std::string fileName)
 void textInit(all_text& text, all_text& font)
 {
 	font.font.loadFromFile("ds-digib.ttf");
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		text.text[i].setFont(font.font);
 		text.text[i].setCharacterSize(30);
 	}
-		text.text[0].setPosition(textPosition);
-		text.text[1].setPosition(textPosition1);
-		text.text[2].setPosition(textPosition2);
-		text.text[3].setPosition(textPosition3);
+	text.text[5].setCharacterSize(70);
+	text.text[0].setPosition(textPosition);
+	text.text[1].setPosition(textPosition1);
+	text.text[2].setPosition(textPosition2);
+	text.text[3].setPosition(textPosition3);
+	text.text[4].setPosition(textPosition4);
+	text.text[5].setPosition(textPosition5);
 }
 
-void PText1(all_text& text, std::int16_t tim, all_text& hp, all_text& point)
+void PText1(all_text& text, std::int16_t tim, all_text& hp, all_text& point, std::int16_t min)
 {
 	text.text[0].setString(std::to_string(tim));
 	text.text[1].setString("Hp");
 	text.text[2].setString(std::to_string(hp.hp));
 	text.text[3].setString(std::to_string(point.point));
+	text.text[4].setString(std::to_string(min));
+	text.text[5].setString("You win");
 }
--- settings.h
+++ settings.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include <SFML/Graphics.hpp>
 
 const float WINDOW_WIDTH = 800;
@@ -9,6 +9,10 @@ const float FPS = 60.f;
 const sf::Vector2f START{ WINDOW_WIDTH / 2, WINDOW_HEIGHT - 550 };
 
 const sf::Vector2f textPosition{ WINDOW_WIDTH - 100.f, 50 };
-const sf::Vector2f textPosition1{ WINDOW_WIDTH - 100.f, 100 };
-const sf::Vector2f textPosition2{ WINDOW_WIDTH - 100.f, 150 };
-const sf::Vector2f textPosition3{ WINDOW_WIDTH - 100.f, 200 };
+const sf::Vector2f textPosition1{ WINDOW_WIDTH - 150.f, 100 };
+const sf::Vector2f textPosition2{ WINDOW_WIDTH - 100.f, 100 };
+const sf::Vector2f textPosition3{ WINDOW_WIDTH - 100.f, 150 };
+const sf::Vector2f textPosition4{ WINDOW_WIDTH - 150.f, 50 };
+const sf::Vector2f textPosition5{ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 };
+const sf::Vector2f Pos { WINDOW_WIDTH - 100.f, 200 };
+const std::string file_Name = "car1.png";
